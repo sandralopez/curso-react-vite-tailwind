6f20c660618a2ced6cf447ba68bb696868f64fc1
sandralopez <slopez52751@gmail.com>
2023-06-23 17:42:42 +0200

    Using React Context. Count items in cart

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+	const context = useContext(ShoppingCartContext)
 	const activeStyle = 'underline underline-offset-4'
 
 	return (
@@ -100,7 +103,7 @@ const Navbar = () => {
 					</NavLink>
 				</li>
 				<li>
-					Cart
+					Cart { context.count }
 				</li>
 			</ul>
 		</nav>
