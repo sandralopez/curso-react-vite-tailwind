bdb8a61d45b7570c1b4ff4048ab2b462d823457f
sandralopez <slopez52751@gmail.com>
2023-06-23 15:53:35 +0200

    Drawing cards with products from API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from 'react'
 import { Layout } from '../../Components/Layout'
 import { Card } from '../../Components/Card'
 
 function Home() {
+  const [items, setItems] = useState(null); 
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
+
     </Layout>
   )
 }
