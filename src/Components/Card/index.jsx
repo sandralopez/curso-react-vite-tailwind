9c9a2cb29176ae68ade6b60f14b41459cde7606f
sandralopez <slopez52751@gmail.com>
2023-06-27 14:24:49 +0200

    Show product detail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,12 +1,19 @@
-import { PlusIcon} from '@heroicons/react/24/solid'
 import { useContext } from 'react' 
+import { PlusIcon} from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
 	const context = useContext(ShoppingCartContext)
 
+	const showProduct = (productDetail) => {
+		context.openProductDetail()
+		context.setProductToShow(productDetail);
+	}
+
 	return (
-		<div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+		<div 
+			className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+			onClick={() => showProduct(data.data)} >
 			<figure className="relative mb-2 w-full h-4/5">
 				<span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category}</span>
 				<img className="w-full h-full object-cover rounded-lg" src={data.data.image} alt={data.data.title} />
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,12 +3,26 @@ import { createContext, useState } from 'react'
 const ShoppingCartContext = createContext()
 
 const ShoppingCartProvider = ({children}) => {
+	// Shopping Cart / Increment quantity
 	const [count, setCount] = useState(0);
+	
+	// Product Detail - Open/Close
+	const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+	const openProductDetail = () => setIsProductDetailOpen(true)
+	const closeProductDetail = () => setIsProductDetailOpen(false)
+
+	// Product Detail - Show product
+	const [productToShow, setProductToShow] = useState({})
 
 	return (
 		<ShoppingCartContext.Provider value={{
 			count,
-			setCount
+			setCount,
+			isProductDetailOpen,
+			openProductDetail,
+			closeProductDetail,
+			productToShow,
+			setProductToShow,
 		}}>
 			{children}
 		</ShoppingCartContext.Provider>
